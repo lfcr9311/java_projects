86e1c00fdfb1038e3850cbe816748a8a1df24b10
Luis Felipe Copetti Rossi <lfcr93@gmail.com>
2023-06-25 04:38:22 -0300

    bubble sort + findNumber

--- Sorts/Sort.java
+++ Sorts/Sort.java
@@ -1,29 +1,40 @@
+package Sorts;
 import java.util.Random;
 
 public class Sort {
     
-    
-    
-    
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         final int array[] = new int[50];
 
         Random random = new Random();
         
         
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(1000);
+            array[i] = random.nextInt(100);
         }
 
         BubbleSort(array);
 
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
+        findNumber(array, 4);
+
+        // for (int i = 0; i < array.length; i++) {
+        //     System.out.println(array[i]);
+        // }
 
 
     }      
 
+    public static int findNumber(int[] array, int number){
+        int aux = -1;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == number){
+                System.out.println("Position: " + i + " Number: "+ number);
+            }
+        }
+        
+        return aux;
+    }
+    
     public static void BubbleSort(int[] array){
         
         for (int i = 0; i < array.length; i++) {
@@ -36,4 +47,9 @@ public class Sort {
             }
             }
         }
+
+    public static void MergeSort(int[] array){
+
+    
+    }
 }
